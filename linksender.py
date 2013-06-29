1ac08bc1065c42f0c325b1249f7ea69faa5853d4
Audrey Roy <audreyr@gmail.com>
2013-06-30 00:01:06 +0200

    Open and write link to index.html.

--- linksender.py
+++ linksender.py
@@ -13,8 +13,12 @@ conn = boto.connect_s3()
 bucket_name = os.environ.get('LINKSENDER_BUCKET')
 bucket = conn.get_bucket(bucket_name)
 
-# TODO...
+# TODO: write command line arg as link
 
 # Create index.html
+f = open('index.html', 'w')
+f.write('<a href="http://pydanny.com">link</a>')
 
-# Upload it to the bucket
+# TODO: Upload index.html to the bucket as public file
+
+# TODO: Open browser to the URL
